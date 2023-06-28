24bb6747ed3528c5fbf9e0a11f789bc881d31ac5
Arthur Rodrigues <arthur.rodrigues@varsolutions.com.br>
2023-06-28 17:23:00 -0300

    Projeto de blog JavaScript

---
+++ js/index.js
@@ -0,0 +1,121 @@
+
+
+const loadingElement = document.querySelector("#loading")
+const postsContainer = document.querySelector("#posts-container")
+const botao = document.querySelector("#botaoBuscar")
+const url = "https://jsonplaceholder.typicode.com/posts"
+
+//Pegar a url
+const urlParametros = new URLSearchParams(window.location.search)
+const IdPost = urlParametros.get("id")
+const comentariosContainer = document.querySelector("#comentarios-container")
+
+const comentarioForm = document.querySelector("#comentarios-form")
+const emailinput = document.querySelector("#email")
+const comentarioinput = document.querySelector("#txt-comentario")
+
+if (!IdPost) {
+    BuscarTodosPosts()
+}
+else {
+    console.log("o valor do Idpost é" + IdPost)
+    BuscarPost(IdPost)
+
+    comentarioForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    let comentarioInserido = {
+        email: emailinput.value,
+        body: comentarioinput.value,
+    }    
+    console.log("comentario antes do tratamento json " + comentarioInserido)
+    comentarioInserido = JSON.stringify(comentarioInserido)
+    console.log("comentario depois do tratamento json " + comentarioInserido)
+    postComentario(comentarioInserido)
+    })
+}
+
+
+async function BuscarTodosPosts() {
+    const resposta = await fetch(url)
+
+console.log(resposta)
+
+const data = await resposta.json()
+
+console.log(data)
+
+loadingElement.classList.add("hide")
+
+data.map((postagem) => {
+const div = document.createElement("div")
+const title = document.createElement("h2")
+const body = document.createElement("p")
+const link = document.createElement("a")
+
+title.innerText = postagem.title
+body.innerText = postagem.body
+link.innerText = "Ler"
+link.setAttribute("href", './post.html?id=' + postagem.id)
+
+div.appendChild(title)
+div.appendChild(body)
+div.appendChild(link)
+postsContainer.appendChild(div)
+})
+}
+
+async function BuscarPost(id) {
+    //const respostaPost = await fetch(`${url}/${id}`) //fetch(url + "/" + id)
+    //const respostaComentario = await fetch(`${url}/${id}/comments`) 
+
+    const [respostaPost, respostaComentario] = await Promise.all ([
+        fetch(`${url}/${id}`), 
+        fetch(`${url}/${id}/comments`),   
+    ])
+
+    const dataPostagem = await respostaPost.json()
+    const dataComentario = await respostaComentario.json()
+    
+    
+    loadingElement.classList.add("hide")
+    const title = document.createElement("h1")
+    const body = document.createElement("p")
+
+    title.innerHTML = dataPostagem.title
+    body.innerText = dataPostagem.body
+
+    postsContainer.appendChild(title)
+    postsContainer.appendChild(body)
+
+    dataComentario.map((comentario) => {
+        criarComentario(comentario)
+    })
+} 
+
+function criarComentario(comentario) {
+    const divComentario = document.createElement("div")
+    const email = document.createElement("h3")
+    const paragrafoComentario = document.createElement("p")
+
+    email.innerText = comentario.email
+    paragrafoComentario.innerText = comentario.body
+
+    divComentario.appendChild(email)
+    divComentario.appendChild(paragrafoComentario)
+    comentariosContainer.appendChild(divComentario)
+}
+
+async function postComentario (comentario)
+{
+    const resposta = await fetch(url, {
+    method: "POST", 
+    body: comentario,
+    headers: {
+        "Content-type": "application/json",
+    }
+})
+const dataResposta = await resposta.json()
+
+criarComentario(dataResposta)
+}
